d019fb9ae505ed45464021ee03c8fd3ce10142e2
Rob <13954303+robertwayne@users.noreply.github.com>
2020-08-17 14:39:26 -0400

    New generate.

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,6 +10,9 @@ export default {
   ** See https://nuxtjs.org/api/configuration-target
   */
   target: 'static',
+  router: {
+    base: '/dpymenus/'
+  },
   /*
   ** Headers of the page
   ** See https://nuxtjs.org/api/configuration-head
