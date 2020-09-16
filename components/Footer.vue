3015f5d0f9ba8aae159f1d74a677651a5b6902d4
Rob Wagner <13954303+robertwayne@users.noreply.github.com>
2020-09-16 05:56:35 -0400

    Changed footer from fixed.

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-  <footer class="fixed h-16 bg-dis-darkest w-full bottom-0 flex items-center justify-center">
+  <footer class="h-16 bg-dis-darkest w-full bottom-0 flex items-center justify-center">
     <div class="flex justify-center w-100 text-center text-dis-grey text-sm">
       Copyright © 2020 Rob Wagner <br />
       Made with ❤️ in New York
