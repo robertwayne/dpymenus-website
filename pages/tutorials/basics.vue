4d02d7aa37471a1ebc73077c7fe03a843bf0bc14
Rob Wagner <13954303+robertwayne@users.noreply.github.com>
2020-10-10 01:31:25 -0400

    Added code lines to inline blocks.

--- pages/tutorials/basics.vue
+++ pages/tutorials/basics.vue
@@ -9,13 +9,20 @@
 </style>
 
 <script>
+import Prism from 'prismjs'
+import 'prismjs/plugins/line-numbers/prism-line-numbers.js'
+import 'prismjs/components/prism-python.js'
+
 export default {
-  async asyncData ({ $content }) {
+  async asyncData({ $content }) {
     const page = await $content('articles/basics_tutorial').fetch()
 
     return {
       page
     }
+  },
+  mounted() {
+    Prism.highlightAll()
   }
 }
 </script>
